d7100da7baa14d60e7ab537934d6330af733ebfd
tosin <akinsemoyintosin1@gmail.com>
2024-01-27 17:54:04 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './styles'
+import {Stats, Navbar, Hero, Business, Billing, CardDeal, Clients, CTA, Testimonials, Footer} from './components/Components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  
+  <div className="w-full bg-primary overflow-hidden">
+    {/* Navbar */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
-  );
-}
+
+    {/* Hero section */}
+    <div className={`bg-primary ${styles.flexStart} ` }>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+
+    {/*  */}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+      <Stats />
+      <Business />
+      <Billing />
+      <CardDeal />
+      <Testimonials />
+      <Clients /> 
+      <CTA /> 
+      <Footer />
+      </div>
+    </div>
+
+  </div>
+);
+
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constant/data";
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav className=" w-full flex justify-between items-center py-6  navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((navLink, i) => (
+          <li
+            key={navLink.id}
+            className={`font-poppins font-normal cursor-pointer text-white ${
+              i === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
+          >
+            <a href={`#${navLink.id}`}>{navLink.title}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+      </div>
+      <div
+        className={`${
+          toggle ? "flex" : "hidden"
+        } p-6 absolute bg-black-gradient top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+      >
+         <ul className="list-none flex flex-col justify-end items-center flex-1">
+          {navLinks.map((navLink, i) => (
+            <li
+              key={navLink.id}
+              className={`font-poppins font-normal cursor-pointer text-white ${
+                i === navLinks.length - 1 ? "mr-0" : "mb-4"
+              }`}
+            >
+              <a href={`#${navLink.id}`}>{navLink.title}</a>
+            </li>
+          ))}
+        </ul>
+
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
